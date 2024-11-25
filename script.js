80ceed4057a340d865c49b07110c9211653c874e
kishore kannan H <146525564+Kishorekannan93@users.noreply.github.com>
2024-11-25 08:35:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ menu.onclick = () => {
 // scroll reveal 
 ScrollReveal({ 
     reset: true,
-    distance:"50px",
+    distance:"80px",
     duration:2000,
     delay:200
 
 });
 ScrollReveal().reveal('.home-content,.heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img,.skills-container,.project-box,.contact form', { origin: 'bottom'})
+ScrollReveal().reveal('.home-img', { origin: 'bottom'})
 ScrollReveal().reveal('.home-content h1', { origin: 'left' });
 ScrollReveal().reveal('.home-content p', { origin: 'right' });
 
